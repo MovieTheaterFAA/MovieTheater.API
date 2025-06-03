725900294cdef97cfda936eb73c87454e38981dd
Ch1mpleo <vietanh051203@gmail.com>
2025-06-03 23:37:23 +0700

    fix: test ci job

--- MovieTheater.API/Controllers/AdminController.cs
+++ MovieTheater.API/Controllers/AdminController.cs
@@ -1,8 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.VisualBasic;
 using MovieTheater.Application.Interfaces;
-using MovieTheater.Application.Services;
 using MovieTheater.Application.Utils;
 using MovieTheater.Domain.DTOs.AdminDTOs;
 using MovieTheater.Domain.DTOs.UserDTOs;
@@ -12,6 +10,7 @@ using MovieTheater.Infrastructure.Interfaces;
 
 namespace MovieTheater.API.Controllers;
 
+// test
 [Route("api/admin")]
 [ApiController]
 public class AdminController : ControllerBase
@@ -19,7 +18,7 @@ public class AdminController : ControllerBase
     private readonly IAdminService _adminService;
     private readonly IClaimsService _claimsService;
 
-    public AdminController(IAdminService adminService,IClaimsService claimsService)
+    public AdminController(IAdminService adminService, IClaimsService claimsService)
     {
         _adminService = adminService;
         _claimsService = claimsService;
@@ -145,8 +144,8 @@ public class AdminController : ControllerBase
 
         try
         {
-            var adminId =  _claimsService.GetCurrentUserId;
-            var result = await _adminService.DeleteEmployeeAsync(id,adminId);
+            var adminId = _claimsService.GetCurrentUserId;
+            var result = await _adminService.DeleteEmployeeAsync(id, adminId);
 
             if (!result)
                 return BadRequest(ApiResult<object>.Failure("400", "Delete failed. No user found with the provided ID."));
