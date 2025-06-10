b9b5ba1763b4099ae4ac97138ad7e82d51431c60
Thanh Hòa <142723413+thanhhoa1309@users.noreply.github.com>
2025-06-10 18:30:08 +0700

    view promotion

--- MovieTheater.Domain/DTOs/PromotionDTOs/PromotionResponseDto.cs
+++ MovieTheater.Domain/DTOs/PromotionDTOs/PromotionResponseDto.cs
@@ -10,8 +10,6 @@ namespace MovieTheater.Domain.DTOs.PromotionDTOs
     {
         public Guid Id { get; set; }
         public string Title { get; set; }
-        public DateTime StartTime { get; set; }
-        public DateTime EndTime { get; set; }
         public decimal DiscountValue { get; set; }
         public string Detail { get; set; }
         public string Image { get; set; }
