350e40847b947373e39519f7ad3254c4343eb50f
Ch1mpleo <vietanh051203@gmail.com>
2025-06-19 16:41:55 +0700

    fix: Make Event.Image nullable in database

--- MovieTheater.Domain/Entities/Event.cs
+++ MovieTheater.Domain/Entities/Event.cs
@@ -6,7 +6,7 @@
         public DateTime StartTime { get; set; }
         public DateTime EndTime { get; set; }
         public string Detail { get; set; }
-        public string Image { get; set; }
+        public string? Image { get; set; }
 
         // Navigation
         public ICollection<Promotion> Promotions { get; set; }
