ff8fc1f8199d6ea3df3eef1e84e65753f2834c6c
Ch1mpleo <vietanh051203@gmail.com>
2025-06-19 15:09:26 +0700

    refactor: Remove Actors and PosterImage from MovieUpdateDto

--- MovieTheater.Domain/DTOs/MovieDTOs/MovieUpdateDto.cs
+++ MovieTheater.Domain/DTOs/MovieDTOs/MovieUpdateDto.cs
@@ -5,26 +5,14 @@ namespace MovieTheater.Domain.DTOs.MovieDTOs
     public class MovieUpdateDto
     {
         public string? Name { get; set; }
-
         public DateTime? FromDate { get; set; }
-
         public DateTime? ToDate { get; set; }
-
-        public List<string>? Actors { get; set; }      // Danh sách dàn cast
-        public List<string>? ActorsUrl { get; set; }   // Đường dẫn tới ảnh của diễn viên
         public string? Director { get; set; }
-
         public int? RunningTime { get; set; }         // Thời lượng chiếu
-
         public string? TrailerUrl { get; set; }
-
         public List<string>? Genres { get; set; }      // Thể loại phim - MovieType
-
         public string? Description { get; set; }
-
-        public string? PosterImage { get; set; }
-
-        public MovieStatus? Status { get; set; }       
+        public MovieStatus? Status { get; set; }
         public float? Rating { get; set; }
     }
 }
