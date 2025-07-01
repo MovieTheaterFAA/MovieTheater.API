d1a6cf75f33779aead5df5a983672f698f5b9769
Ch1mpleo <vietanh051203@gmail.com>
2025-07-01 14:58:53 +0700

    chore: Add https://localhost:3001 to CORS origins

--- MovieTheater.API/Program.cs
+++ MovieTheater.API/Program.cs
@@ -1,11 +1,11 @@
-﻿using System.IdentityModel.Tokens.Jwt;
-using System.Text.Json;
-using System.Text.Json.Serialization;
-using MovieTheater.API.Architecture;
+﻿using MovieTheater.API.Architecture;
 using MovieTheater.Application.Interfaces;
 using MovieTheater.Application.Services;
 using MovieTheater.Infrastructure.Hubs;
 using SwaggerThemes;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -23,7 +23,8 @@ builder.Services.AddCors(options =>
             policy.WithOrigins(
                 "https://movietheaterfe.ae-tao-fullstack-api.site", // Production
                 "http://localhost:3000",                             // Local dev
-                "http://localhost:3001"                             // Local dev
+                "http://localhost:3001",                             // Local dev
+                "https://localhost:3001"
             )
             .AllowAnyHeader()
             .AllowAnyMethod()
