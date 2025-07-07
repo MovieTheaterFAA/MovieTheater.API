bf3354b40dfb0862668f6cfac0de96c21ba4399f
Ch1mpleo <vietanh051203@gmail.com>
2025-07-07 21:31:19 +0700

    fix: Enable Swagger UI in production environment

--- MovieTheater.API/Program.cs
+++ MovieTheater.API/Program.cs
@@ -116,7 +116,7 @@ using (var scope = app.Services.CreateScope())
 app.UseCors("AllowFrontend");
 
 // Configure the HTTP request pipeline
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI(c =>
