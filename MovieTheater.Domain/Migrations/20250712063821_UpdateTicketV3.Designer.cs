03f2a4fd57cd72c434c5f6bb403098ab6ff61961
Ch1mpleo <vietanh051203@gmail.com>
2025-07-12 13:38:42 +0700

    feat: Update Ticket entity to allow nullable BookingId

--- MovieTheater.Domain/Migrations/20250712063821_UpdateTicketV3.Designer.cs
+++ MovieTheater.Domain/Migrations/20250712063821_UpdateTicketV3.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MovieTheater.Domain;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
@@ -12,9 +13,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace MovieTheater.Domain.Migrations
 {
     [DbContext(typeof(MovieTheaterDbContext))]
-    partial class MovieTheaterDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250712063821_UpdateTicketV3")]
+    partial class UpdateTicketV3
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
