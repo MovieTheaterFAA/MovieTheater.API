bcfefcd046e7ef57f2bdac36ea1d2a0d6281cb4d
Ch1mpleo <vietanh051203@gmail.com>
2025-07-22 22:22:36 +0700

    refactor: Make movie properties nullable

---
+++ MovieTheater.Domain/Migrations/20250722152217_FixMovieV1.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MovieTheater.Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixMovieV1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "PosterImage",
+                table: "Movies",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BackgroundImage",
+                table: "Movies",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<List<string>>(
+                name: "ActorsUrl",
+                table: "Movies",
+                type: "text[]",
+                nullable: true,
+                oldClrType: typeof(List<string>),
+                oldType: "text[]");
+
+            migrationBuilder.AlterColumn<List<string>>(
+                name: "Actors",
+                table: "Movies",
+                type: "text[]",
+                nullable: true,
+                oldClrType: typeof(List<string>),
+                oldType: "text[]");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "PosterImage",
+                table: "Movies",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BackgroundImage",
+                table: "Movies",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<List<string>>(
+                name: "ActorsUrl",
+                table: "Movies",
+                type: "text[]",
+                nullable: false,
+                oldClrType: typeof(List<string>),
+                oldType: "text[]",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<List<string>>(
+                name: "Actors",
+                table: "Movies",
+                type: "text[]",
+                nullable: false,
+                oldClrType: typeof(List<string>),
+                oldType: "text[]",
+                oldNullable: true);
+        }
+    }
+}
